2047808ad089d347a7434967f4d1e604dd7b7a4c
ashhad <125950620+aelricc@users.noreply.github.com>
2026-03-04 15:42:13 +0000

    "my new program"

---
+++ idcard.py
@@ -0,0 +1,12 @@
+#Code to display an ID card.
+def id_card():
+    name = input("Enter your name.")
+    age = input("How old are you?")
+    fav_color = input("What's your favorite color?")
+
+    print("Name: " + name)
+    print("Age: " + age)
+    print("Favorite Color: " + fav_color)
+
+
+#id_card()
